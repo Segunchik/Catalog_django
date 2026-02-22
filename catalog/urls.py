af90d45c2d467bcdfc96256b00912321f761b059
Sergey Parkhanovich <komrad.segunchik@yandex.ru>
2026-02-22 20:25:16 +0300

    add controller home

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,7 +6,7 @@ from catalog.views import home, contacts
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("",home, name="home"),
+    path("/",home, name="home"),
     path("contacts/",contacts, name="contacts"),
 
 ]
